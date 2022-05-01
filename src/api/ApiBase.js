8d2345df4d0cb7c0034f1a2c5d2b3c646d3c874a
bleartdemaku <bd47165@ubt-uni.net>
2022-05-01 16:26:36 +0200

    apiBased returned to normal

--- src/api/ApiBase.js
+++ src/api/ApiBase.js
@@ -8,16 +8,8 @@ if (API_URL.includes('localhost')) {
   API_URL = API_URL.replace('localhost', manifest.debuggerHost.split(':').shift());
 }
 
-/*const Client = axios.create({
-  baseURL: API_URL,
-  timeout: 10000,
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
-});*/
 const Client = axios.create({
-  baseURL: 'http://ec9f-194-6-247-0.ngrok.io',
+  baseURL: API_URL,
   timeout: 10000,
   headers: {
     Accept: 'application/json',
