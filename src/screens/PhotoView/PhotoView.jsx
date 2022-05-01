c01cf1ee8186c951e1cec7f45a9f5de81d5b43c1
rilind nuha <rn48707@ubt-uni.net>
2022-05-01 19:18:26 +0200

    feat: photo-viewer screen

---
+++ src/screens/PhotoView/PhotoView.jsx
@@ -0,0 +1,33 @@
+import { AntDesign } from '@expo/vector-icons';
+import React from 'react';
+import { View, TouchableOpacity, Image } from 'react-native';
+
+import Colors from '../../constants/Colors';
+import styles from './PhotoView.styles';
+
+export default function CameraRN({ route, navigation }) {
+  const photo = route.params?.photo;
+
+  console.log('firdsdfst', photo);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}
+        style={styles.closeButton}
+        activeOpacity={0.7}>
+        <AntDesign name="close" size={32} color={Colors.light} />
+      </TouchableOpacity>
+      <View style={styles.previewContainer}>
+        <Image
+          source={{
+            uri: photo,
+          }}
+          style={styles.photo}
+        />
+      </View>
+    </View>
+  );
+}
