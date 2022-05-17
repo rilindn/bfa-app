72f390048a2f16df6b934626f847ddd8111c7723
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-05-17 20:32:48 +0200

    feat: suggestions cards UI

---
+++ src/components/ClubBottomSection/Suggests/Suggestions/Suggestions.styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+import Colors from '../../../../constants/Colors';
+import { fontSizes } from '../../../../constants/Typography';
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 12,
+    borderRadius: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.headerBg,
+    padding: 5,
+  },
+  suggestedPlayers: {
+    color: Colors.white,
+    fontFamily: 'poppins-semibold',
+    fontSize: fontSizes.large,
+    marginTop: 10,
+  },
+  people: {
+    fontFamily: 'poppins-regular',
+    color: Colors.gray3 + '60',
+  },
+});
+export default styles;
