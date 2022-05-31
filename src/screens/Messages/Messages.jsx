4e26ee5f9ee85b80d17235ae7ad829488ab61bff
bleartdemaku <bd47165@ubt-uni.net>
2022-05-31 12:08:03 +0200

    messages screen

--- src/screens/Messages/Messages.jsx
+++ src/screens/Messages/Messages.jsx
@@ -1,13 +1,36 @@
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Searchbar } from 'react-native-paper';
 
+import Avatar from '../../components/Avatar/Avatar';
+import { fontSizes } from '../../constants/Typography';
+import useAuthContext from '../../hooks/useAuth';
 import styles from './Messages.styles';
 
-export default function Messages() {
+export default function Messages({ name }) {
+  const { authData } = useAuthContext();
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Messages</Text>
-      <Text style={styles.text}>This is messages screen</Text>
-    </SafeAreaView>
+    <ScrollView style={styles.container}>
+      <Searchbar placeholder="Search" />
+      <View style={styles.content}>
+        <Avatar
+          name={name}
+          size={50}
+          initialStyle={{ fontSize: fontSizes.large }}
+          image="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+        />
+        <View style={styles.dataContent}>
+          <Text style={styles.name}>
+            {authData.Player.firstName} {authData.Player.lastName}
+          </Text>
+          <Text style={styles.message}>How are you?</Text>
+        </View>
+
+        <View style={styles.datetimeWrapper}>
+          <Text style={styles.datetime}>06/03/2022 at 16:20</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
--- src/screens/Messages/Messages.styles.js
+++ src/screens/Messages/Messages.styles.js
@@ -1,20 +1,45 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 import Colors from '../../constants/Colors';
+import { fontSizes } from '../../constants/Typography';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    width: Dimensions.get('window').width,
+    padding: 15,
+    backgroundColor: Colors.background,
+  },
+  content: {
+    marginTop: 20,
+    padding: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  dataContent: {
+    marginLeft: 10,
+    flexDirection: 'column',
+    marginRight: 80,
+    alignContent: 'center',
+    alignItems: 'flex-start',
+    alignSelf: 'center',
+  },
+  name: {
+    fontSize: fontSizes.small,
+    color: Colors.white,
+  },
+  message: {
+    fontSize: fontSizes.extraSmall,
+    color: Colors.white,
+  },
+  datetimeWrapper: {
+    alignContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
+    alignSelf: 'center',
   },
-  title: {
-    fontSize: 20,
-    color: Colors.text,
-    fontWeight: 'bold',
-  },
-  text: {
-    color: Colors.text,
+  datetime: {
+    color: Colors.gray3,
+    fontSize: fontSizes.extraSmall,
   },
 });
 
