5095284270ba3b7745d7aa02d5991f62298a195d
rilind nuha <rn48707@ubt-uni.net>
2022-05-31 22:49:56 +0200

    fix: revert base api config, refactor messages screen

---
+++ src/components/Messages/SingleConversation/SingleConversation.styles.js
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+import Colors from '../../../constants/Colors';
+import { fontSizes } from '../../../constants/Typography';
+
+const styles = StyleSheet.create({
+  drawer: {
+    width: '100%',
+  },
+  conversationRow: {
+    height: 60,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: Colors.background,
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  userData: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  userName: {
+    color: Colors.light,
+    fontFamily: 'poppins-semibold',
+    fontSize: fontSizes.medium,
+  },
+  messageText: {
+    color: Colors.light,
+    fontSize: fontSizes.default,
+  },
+  dateTime: {
+    color: Colors.light,
+    fontSize: fontSizes.extraSmall,
+  },
+});
+
+export default styles;
--- src/screens/Messages/Messages.styles.js
+++ src/screens/Messages/Messages.styles.js
@@ -10,50 +10,8 @@ const styles = StyleSheet.create({
     padding: 15,
     backgroundColor: Colors.background,
   },
-  drawer: {
-    height: 60,
+  chat: {
     marginTop: 20,
-    //padding: 0,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  content: {
-    backgroundColor: Colors.background,
-    height: '100%',
-    //marginLeft: 5,
-    paddingLeft: 5,
-    paddingRight: 5,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignContent: 'center',
-    alignItems: 'center',
-  },
-  dataContent: {
-    marginLeft: 10,
-    flexDirection: 'column',
-    marginRight: 70,
-    alignContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-  },
-  name: {
-    fontFamily: 'poppins-bold',
-    fontSize: fontSizes.small,
-    color: Colors.white,
-  },
-  message: {
-    fontSize: fontSizes.extraSmall,
-    color: Colors.white,
-    alignSelf: 'flex-start',
-  },
-  datetimeWrapper: {
-    alignContent: 'center',
-    alignItems: 'center',
-    alignSelf: 'center',
-  },
-  datetime: {
-    color: Colors.gray3,
-    fontSize: fontSizes.extraSmall,
   },
 });
 
