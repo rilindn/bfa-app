7601e70a70b9f8b801d658003540589ef2293cc6
bleartdemaku <bd47165@ubt-uni.net>
2022-06-19 23:38:26 +0200

    Notification Screen

--- src/screens/Notifications/Notifications.jsx
+++ src/screens/Notifications/Notifications.jsx
@@ -1,13 +1,25 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
+import Avatar from '../../components/Avatar/Avatar';
+import fontSizes from '../../constants/Typography';
 import styles from './Notifications.styles';
 
 export default function Notifications() {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Notifications</Text>
-      <Text style={styles.text}>This is notifications screen</Text>
+      <View style={styles.content}>
+        <Avatar
+          size={50}
+          initialStyle={{ fontSize: fontSizes.large }}
+          image="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80"
+        />
+        <View style={styles.dataContent}>
+          <Text style={styles.name}>We posted something new!</Text>
+          <Text style={styles.datetime}>06/03/2022</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/Notifications/Notifications.styles.js
+++ src/screens/Notifications/Notifications.styles.js
@@ -1,20 +1,50 @@
 import { StyleSheet } from 'react-native';
 
 import Colors from '../../constants/Colors';
+import { fontSizes } from '../../constants/Typography';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    backgroundColor: Colors.background,
   },
   title: {
-    fontSize: 20,
-    color: Colors.text,
+    fontSize: fontSizes.title,
+    color: Colors.light,
     fontWeight: 'bold',
+    textAlign: 'center',
+    borderBottomWidth: 1,
+    borderColor: Colors.gray3 + '50',
+    width: '100%',
+    paddingBottom: 10,
   },
-  text: {
-    color: Colors.text,
+  content: {
+    backgroundColor: Colors.gray6,
+    flexDirection: 'row',
+    borderWidth: 1,
+    borderColor: Colors.gray3 + '50',
+    borderRadius: 15,
+    marginTop: 30,
+    width: '95%',
+    padding: 10,
+    alignContent: 'center',
+    alignItems: 'center',
+  },
+  dataContent: {
+    marginLeft: 10,
+    flexDirection: 'column',
+    alignContent: 'center',
+    alignSelf: 'center',
+  },
+  name: {
+    fontSize: fontSizes.default,
+    color: Colors.white,
+  },
+  datetime: {
+    color: Colors.gray3,
+    fontSize: fontSizes.extraSmall,
   },
 });
 export default styles;
